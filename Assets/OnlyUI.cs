656a1a9080431a4c48141a00f56e852c881e6884
Martin <martybalch21@live.com>
2018-01-28 02:03:36 -0800

    started adding UI/UX

---
+++ Assets/OnlyUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnlyUI : MonoBehaviour
+{
+    public Text tip;
+    public Text tip2;
+
+    public Image win;
+
+    public float timer = 5;
+    public float timer2 = 5;
+
+    bool hint = false;
+
+    // Update is called once per frame
+
+    void Start()
+    {
+        tip2.gameObject.SetActive(false);    
+    }
+    void Update ()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            tip.gameObject.SetActive(false);
+            hint = true;
+        }
+        if (hint == true)
+        {
+            timer2 -= Time.deltaTime;
+            tip2.gameObject.SetActive(true);
+        }
+        if (timer2 <= 0)
+        {
+            tip2.gameObject.SetActive(false);
+        }
+		
+	}
+}
---
+++ Assets/menuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuManager : MonoBehaviour
+{
+
+    public Button start;
+    public Button quit;
+
+    public void startPress()
+    {
+        SceneManager.LoadScene("Beta");
+    }
+
+    public void quitPress()
+    {
+        Application.Quit();
+    }
+}
